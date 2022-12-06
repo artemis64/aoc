b957e5056ca177e284a5d04465839760ddc421b1
Anna Šuštar <anicka.sustar@gmail.com>
2022-12-06 09:49:15 +0100

    Advent 6 2022

---
+++ 2022/advent6a.py
@@ -0,0 +1,15 @@
+file = open('input6.txt', 'r')
+
+for line in file:
+	line = line.strip()
+	four = ''
+	for index in range(len(line)):
+		if not line[index] in four:
+			four = four + line[index]
+			if len(four) == 4:
+				print(index + 1)
+				break
+		else:
+			sameIndex = four.index(line[index])
+			four = four[(sameIndex + 1):] + line[index]
+
